5eb1aebb72bfeed8f71a2c3821d666d713be0b92
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-07-19 18:56:49 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,20 @@
 # Create your costume_stores migration here
+require "pry"
+class CostumeStore < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory 
+      t.integer :number_of_employees
+      t.integer :still_in_business
+      t.string :opening_time 
+      t.string :closing_time
+      binding.pry
+
+    end
+  
+  
+
+end
