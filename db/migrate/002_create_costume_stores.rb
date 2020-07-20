3c8b7697f236f57c2d87f2bf5af04e6d26668d8e
RachelCodesToo <a.r.fausto@gmail.com>
2020-07-20 18:43:28 -0400

    .

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create youlear costume_stores migration he
-class CreateCostumeStores < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
     def change
         create_table :costume_stores do |t|
             t.string :name 
