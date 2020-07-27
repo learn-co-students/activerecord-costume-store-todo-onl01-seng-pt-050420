6f0a5f27e1e6994ddb25a01e30b7ce0aca078931
tpham13 <tpham0123@gmail.com>
2020-07-27 12:34:46 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CostumeStore
+    create_table :costume_stores do |t|
+        t.string :name
+        t.string :location
+        t.integer :number of costumes 
+        t.integer :number of employees
+        t.boolean :still_in_business
+        t.datetime :opening_time
+        t.datetime :closing_time
+
+    end 
+
+end
