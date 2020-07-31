a9cc23c7d207dba911faadbfd6c7a06c00b0cf82
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-07-31 14:05:07 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -2,10 +2,5 @@
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
 
-
-
-    create_table :costumes do |t|
-        t.string :name
-    end
     
 end
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,15 +8,16 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-class Costume < ActiveRecord::Migration[5.2]
+class CreateCostumes < ActiveRecord::Migration[5.2]
 
-
-
-    # create_table :costumes do |t|
-    #     t.string :name
-    #     t.integer :price
-    #     t.string :size
-    #     t.string :image_url
-    #   end
-    
+    def change
+        create_table :costumes do |t|
+          t.string :name
+          t.integer :price
+          t.string :size
+          t.string :image_url
+          t.timestamps
+        end
     end
+    
+end
